494a61284c3e4933fbcfac6a46affc984c9e7b36
JohnL4 <john-github@how-hard-can-it-be.com>
2017-03-28 13:04:15 -0700

    Add session-ops component to main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ClusterPersistenceService } from './cluster-persistence.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
 
+  constructor( private _persistenceSvc: ClusterPersistenceService) {}
+
+  ngOnInit()
+  {
+    this._persistenceSvc.init();
+    this._persistenceSvc.connectToDatabase();
+  }
 }
